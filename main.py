2084d70bd1b15c25f0c1d54fd0fe5a2c13b09a8c
Raisa <Ricefactory237@gmail.com>
2026-06-21 18:15:51 +0800

    servo motor updated

--- main.py
+++ main.py
@@ -3,17 +3,16 @@ from time import sleep
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11, GPIO.OUT)
-pwm=GPIO.PWM(11, 50)
+pwm=GPIO.PWM(11, 50) #GPIO 14, ground and 5v
 pwm.start(0)
-pwm.ChangeDutyCycle(5) # left -90 deg position
+pwm.ChangeDutyCycle(5) # left -45?
 sleep(1)
 pwm.ChangeDutyCycle(7.5) # neutral position
 sleep(1)
-pwm.ChangeDutyCycle(10) # right +90 deg position
+pwm.ChangeDutyCycle(10) # right +45 ?
 sleep(1)
 
 print("hello worlddd")
-print("test 2")
 
-pwm.stop()
+pwm.stop()  #GPIO servo motor works
 GPIO.cleanup()
