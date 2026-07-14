a8be590a6202a67709052c869050788188549f07
Raisa <Ricefactory237@gmail.com>
2026-07-14 17:44:20 +0800

    add pir

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD) #GPI.BOARD
 GPIO.setup(29, GPIO.OUT) #servo
-#GPIO.setup(7,GPIO.IN) # pir sensor
+GPIO.setup(7,GPIO.IN) # pir sensor, GPIO4
 #GPIO.setup(11, GPIO.OUT) #ir led/rgb
 #GPIO.setup(7, GPIO.IN)  #ir reciever
 
@@ -62,33 +62,28 @@ while True:
 
     pwm.stop()  #GPIO servo motor works
     print("pwm off")
+    print("pir")
+    '''
+    while True:
+        i = GPIO.input(7) #get resut of input
+        if i == 1:
+            print("got 1") #movement detected
+        elif i == 0:
+            print("got 0") #movement not detected
+        time.sleep(0.2) #0.2 second pause cause why not
+    '''
+    pwm = GPIO.PWM(7, 38000) #og 11
+    pwm.start(50)  # 50% duty cycle
 
-  
-"""
-while True:
-    i = GPIO.input(7) #get resut of input
-    if i == 1:
-        print("got 1") #movement detected
-    elif i == 0:
-        print("got 0") #movement not detected
-    time.sleep(0.2) #0.2 second pause cause why not
-"""
+    print("IR transmitter ON (38kHz)")
 
-print("hello worlddd")
+    while True:
+            if GPIO.input(7) == 0:
+                print("detected")
+            elif GPIO.input(7) == 1:
+                print("nothin!")
+            time.sleep(0.05)
 
-"""
-pwm = GPIO.PWM(11, 38000)
-pwm.start(50)  # 50% duty cycle
-
-print("IR transmitter ON (38kHz)")
-
-while True:
-        if GPIO.input(7) == 0:
-            print("detected")
-        elif GPIO.input(7) == 1:
-            print("nothin!")
-        time.sleep(0.05)
-"""
 GPIO.cleanup() #general exit statement
 
 
